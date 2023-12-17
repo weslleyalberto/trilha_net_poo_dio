f9e125d9de1ebd69c61050b57e02240c703b6269
Weslley Alberto <weslleyalberto@gmail.com>
2023-12-17 16:11:48 -0300

    Projeto Concluido!

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string iMEI, int memoria) : base(numero, modelo, iMEI, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Aplicativo instalado {nomeApp} no IPhone com sucesso!");
+        }
+       
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia("5454", "5200", "5446545456", 256);
+Smartphone iphone = new Iphone("6", "6S", "5445465477", 64);
+
+nokia.InstalarAplicativo("Twitter");
+iphone.InstalarAplicativo("Chrome");
+nokia.ReceberLigacao();
+iphone.ReceberLigacao();
+nokia.Ligar();
+iphone.Ligar(); 
